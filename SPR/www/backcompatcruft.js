0020b569fa209d75338f55c7644818c402f81d3c
Masa Yano <yanomasa813@gmail.com>
2021-08-27 13:50:10 +0900

    .

--- SPR/www/backcompatcruft.js
+++
@@ -1,7 +0,0 @@
-var Separator = "Separator";
-var Message = "Message";
-var Question = "Question";
-var DashedSentence = "DashedSentence";
-var AcceptabilityJudgment = "AcceptabilityJudgment";
-var FlashSentence = "FlashSentence";
-var VBox = "VBox";
